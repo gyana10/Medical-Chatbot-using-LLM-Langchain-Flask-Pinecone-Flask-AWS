a29359c2cd326aed35f2ea1af7ac63508345f331
Gyana Ranjan Mohanty <gyanarmohanty20@gmail.com>
2025-10-06 16:51:49 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+setup(
+    name="Medical-Chatbot",
+    version="0.1.0",
+    author="Gyana Ranjan Mohanty",
+    author_email="gyanarmohanty20@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
